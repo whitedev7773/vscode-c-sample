4a5821583e6a08948211fbb222b8ca542ccd8f03
Giwon <jkw04257773@gmail.com>
2024-09-04 00:01:39 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello World From VSCode C Sample");
+  printf("ECU-KR 인코딩으로 생성했습니다.");
+  printf(".vscode 폴더는 지우지 마세요. 폴더별로 적용되는 설정파일입니다.");
+  return 0;
+}

914fc815e8530a6eb6272d72abecb972921d2549
Giwon <jkw04257773@gmail.com>
2024-11-12 15:41:00 +0900

    feat: 빌드 후 실행 시 명령 인자 input 추가

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
   printf("Hello World From VSCode C Sample\n");
   printf("ECU-KR 인코딩으로 생성했습니다.\n");
   printf(".vscode 폴더는 지우지 마세요. 폴더별로 적용되는 설정파일입니다.\n");
+
+  printf("\n");
+
+  // 인자 출력
+  printf("argc = %d\n\n", argc);
+  for (int i = 0; i < argc; i++) {
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
   return 0;
 }
